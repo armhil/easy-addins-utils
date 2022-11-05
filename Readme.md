4ab691637bf7e463772cf0f93fd86c0b9eba2dd4
armhil <arman.hilmioglu@gmail.com>
2022-11-05 21:20:49 +0000

    Add a new test, update the Readme.md

--- Readme.md
+++ Readme.md
@@ -1,5 +1,7 @@
 # easy-addins-utils
 
+[![Main CI | Build, Test and NPM Publish](https://github.com/armhil/easy-addins-utils/actions/workflows/main.yml/badge.svg)](https://github.com/armhil/easy-addins-utils/actions/workflows/main.yml)
+
 A collection of add-in functionalities (to the best of my abilities, cross-platform between Office and G-suite - a lot of the G-suite things are not built yet).
 
 This library is used by my addins.
--- src/__tests__/addin-utils.tests.ts
+++ src/__tests__/addin-utils.tests.ts
@@ -73,4 +73,20 @@ describe('AddinUtils tests', () => {
     mockFn.mock.lastCall[1]();
     expect(successFn).toHaveBeenCalledTimes(1);
   });
+
+  it('InsertImage:Office calls setSelectedDataAsync with correct params', () => {
+    const mockFn = jest.fn();
+    const successFn = jest.fn();
+    const imageText = 'imageText';
+    window.Office =  { CoercionType: { Image: 'Image' }, context : { document: { setSelectedDataAsync: mockFn }}};
+
+    AddinUtils.InsertImage(imageText, successFn);
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    expect(mockFn.mock.lastCall[0]).toBe(imageText);
+    expect(mockFn.mock.lastCall[1]).toEqual({coercionType: window.Office.CoercionType.Image});
+    expect(successFn).toHaveBeenCalledTimes(0);
+    // manually call the callback of InsertImage - which should call our call-back
+    mockFn.mock.lastCall[2]();
+    expect(successFn).toHaveBeenCalledTimes(1);
+  });
 });
